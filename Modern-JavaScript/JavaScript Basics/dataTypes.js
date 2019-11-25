652830a77dd2ed9de0353c11406b3c8a06a1f7ff
Kat <littlepawcoding@gmail.com>
2019-11-25 15:18:00 +0000

    added new folder for tidy directory

---
+++ Modern-JavaScript/JavaScript Basics/dataTypes.js
@@ -0,0 +1,11 @@
+// Data Types at a Glance.
+
+// Numbers - all numbers, including decimals
+// Strings - usually text, surrounded by " " or ' '
+// Boolean - true or false
+// Null - variable that has no value that is purposly set by us
+// Undefined - empty values but given automatically
+// Objects - arrays, object literals
+// Symbol
+
+// JS loosly typed language, meaning the code would most likely run even with small syntax errors like forgetting a semi colon ;
---
+++ Modern-JavaScript/JavaScript Basics/strings.js
@@ -0,0 +1,15 @@
+// STRINGS
+
+// examples
+console.log("Hello, Dug");
+
+// string concatenation
+
+// getting characters
+
+// string length
+
+// string methods
+
+// Notes:
+// - can use either " " or ' ' but make sure you are consistent
