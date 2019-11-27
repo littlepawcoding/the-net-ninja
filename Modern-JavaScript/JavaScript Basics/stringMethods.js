8a185d94e5d622c7d22f7e36cb23728675af3550
Kat <littlepawcoding@gmail.com>
2019-11-27 13:16:07 +0000

    updating content

---
+++ Modern-JavaScript/JavaScript Basics/stringMethods.js
@@ -0,0 +1,15 @@
+// COMMON STRING METHODS
+
+let email = "dug@thepupdug.co.uk";
+
+let result = email.lastIndexOf("t");
+console.log(result); // 4
+
+// NOTES:
+/*
+- methods are just functions that belong to a specific object
+- we want to find the last occuring index of (whatever we put), in this case its the string "t".
+    . we do that by using the method .lastIndexOf
+    . the result of the method we store in a variable called result, so we just need to call result in the . console.log later.
+- 
+*/
