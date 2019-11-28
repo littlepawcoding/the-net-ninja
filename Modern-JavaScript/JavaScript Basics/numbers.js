045ab85840f42de7e25eb43537fc1a6ca334b6fc
Kat <littlepawcoding@gmail.com>
2019-11-28 14:28:51 +0000

    updated html for live server and update notes for numbers.js

--- Modern-JavaScript/JavaScript Basics/numbers.js
+++ Modern-JavaScript/JavaScript Basics/numbers.js
@@ -1,6 +1,27 @@
 // NUMBERS
 
+let radius = 10;
+const pi = 3.14;
+
+// math operators
+// + plus
+// - minus
+// * multiply
+// / divide
+// ** the power of
+// % remainder of
+
+// let result = radius % 3; // 1
+
+// let result = pi * radius ** 2; // 314
+
+// order of operation - how we perform calculations in
+
+console.log(result);
+
 // NOTES:
 /*
-
+- example we want to find out the remainder of radius by 3
+- we use % to find the remainder
+- let result = pi * radius ** 2; work out the area of a circle 
 */
