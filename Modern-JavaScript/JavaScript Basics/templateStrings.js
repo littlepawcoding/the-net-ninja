51e2b80160c28634d780186b8a04c771f5dfe75a
Kat <littlepawcoding@gmail.com>
2019-11-28 19:07:13 +0000

    templateString lecture complete - notes updated

--- Modern-JavaScript/JavaScript Basics/templateStrings.js
+++ Modern-JavaScript/JavaScript Basics/templateStrings.js
@@ -1,7 +1,31 @@
 // TEMPLATE STRINGS
+const title = "Best of Dug 2019";
+const author = "Dug";
+const likes = 1000;
+
+// concatenation way
+let result =
+  title + " written by " + author + " (" + likes + " people like this)";
+// Best of Dug 2019 written by Dug (1000 people like this)
+console.log(result);
+
+// template string way
+let answer = `${title} written by ${author} (${likes} people like this)`;
+// Best of Dug 2019 written by Dug (1000 people like this)
+console.log(answer);
+
+// html templates
+let html = `
+<h2>${title}</h2>
+<p>written by ${author}</p>
+<p>${likes} people like this</p>
+`;
+console.log(html);
 
 // NOTES:
 /*
-- 
-
+- not quite a new data type but more like a different version of a string
+- template strings are also known as template literals
+- allows us to inject variables directly into the string without having to come out of it
+- can use template strings to output some content to the browser, explains more later on in the course
 */
