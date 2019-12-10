eae8d8d0b262a61dfcbd73d5be3b8fc838bd5996
Kat <littlepawcoding@gmail.com>
2019-12-10 21:21:49 +0000

    updated html for live server

---
+++ Modern-JavaScript/Control Flow Basics/lectures/forLoops.js
@@ -0,0 +1,16 @@
+// FOR LOOPS
+
+for (let i = 0; i < 5; i++) {
+  console.log("loop..", i);
+}
+console.log("loop finished");
+/* NOTES:
+- many loops in js
+- don't need ; to close a loop
+- let i = 0; known as a counter
+- i < 5; known as condition
+- if true execute the code
+- i++ 
+- don't usually know how many times to loop through, usually check through a chunk of data
+
+*/
