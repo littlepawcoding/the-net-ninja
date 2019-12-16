174c5e370aef49ce58e2b429c48e9b6df119801e
Kat <littlepawcoding@gmail.com>
2019-12-16 11:03:12 +0000

    do while loop added & completed - notes updated

---
+++ Modern-JavaScript/Control Flow Basics/lectures/doWhileLoops.js
@@ -0,0 +1,17 @@
+// DO WHILE LOOPS
+
+let i = 5;
+
+do {
+  console.log("i is:", i);
+  i++;
+} while (i < 5);
+
+// NOTES:
+/*
+- again, the same as FOR and WHILE with one difference
+- a do while loop is an extension of a while loop
+- the do part allows the code to run ONCE, regardless, even if statement is false
+- code is ran once, then will look to while statement and work out if true or not
+- if true it will run, if not it will not, same as a normal loop
+*/
