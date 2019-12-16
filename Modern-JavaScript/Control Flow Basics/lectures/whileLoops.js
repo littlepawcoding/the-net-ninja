ea44f867d1f71a04218aeded06832fc33f55f786
Kat <littlepawcoding@gmail.com>
2019-12-16 10:56:05 +0000

    while loop lecture completed - notes updated

--- Modern-JavaScript/Control Flow Basics/lectures/whileLoops.js
+++ Modern-JavaScript/Control Flow Basics/lectures/whileLoops.js
@@ -1,6 +1,26 @@
 // WHILE LOOPS
 
+let i = 0;
+
+while (i < 5) {
+  console.log("loop: ", i);
+  i++;
+}
+
+const names = ["Dug", "Binki", "Kanga"];
+
+let j = 0;
+
+while (j < names.length) {
+  console.log(names[j]);
+  j++;
+}
+
 // NOTES:
 /*
-
+- similar to FOR loops, it loops through a chunk of code a certain amount of times.
+- syntax differences
+- we set i outside of the code block
+- the while only hold the condition
+- increment is inside of the loop
 */
