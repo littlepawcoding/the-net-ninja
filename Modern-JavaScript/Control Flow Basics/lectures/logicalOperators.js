f73aa992e13544796816014a648c4f94f2affd14
Kat <littlepawcoding@gmail.com>
2019-12-20 17:56:04 +0000

    logical operators lecture complete - notes updated

---
+++ Modern-JavaScript/Control Flow Basics/lectures/logicalOperators.js
@@ -0,0 +1,18 @@
+// LOGICAL OPERATORS
+
+// OR ||
+// AND &&
+
+const password = "p@ssword";
+
+if (password.length >= 8 && password.includes("@")) {
+  console.log("great password");
+} else {
+  console.log("please improve password");
+}
+
+// NOTES:
+/*
+- AND && if something is this AND that then code block will fire
+- OR || if something is this OR that but only one has to pass for the code block to fire
+*/
