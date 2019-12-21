4be214fba00c426acb8559f93b045ba168e137c9
Kat <littlepawcoding@gmail.com>
2019-12-21 15:46:11 +0000

    logical not lecture complete

--- Modern-JavaScript/Control Flow Basics/lectures/logicalNot.js
+++ Modern-JavaScript/Control Flow Basics/lectures/logicalNot.js
@@ -1,6 +1,22 @@
 // LOGICAL NOT (!)
 
-/*
+let user = false;
+
+if (!user) {
+  console.log("You are not logged in!");
+}
+
+console.log(true); // true
+console.log(!true); // false
+console.log(false); // false
+console.log(!false); // true
+
 // NOTES:
+/*
+- logical not is represented by the exclamation sign !
+- automatically converts value to a boolean and then returns the inverted
+- has a high precidence than OR / AND
+- only run code if false, best example is if user is logged in, if false we want to send an message saying not logged in
+
 
 */
