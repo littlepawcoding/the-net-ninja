3bd5047d1c2ad4264e96f76f28aa7d856103edc3
Kat <littlepawcoding@gmail.com>
2019-12-21 20:42:31 +0000

    switch file added - html updated

---
+++ Modern-JavaScript/Control Flow Basics/lectures/switch.js
@@ -0,0 +1,8 @@
+// SWITCH STATEMENTS
+
+// NOTES:
+/*
+- switch statements are similar to else if, except you can have all the cases within one code block
+- grading system is the best example
+- we want to create a system that matches the given grade with the list of grades and print the correct output
+*/
