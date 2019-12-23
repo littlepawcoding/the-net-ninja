0f013de9c7a5f6dfb4cb512e7bcd3c82588be365
Kat <littlepawcoding@gmail.com>
2019-12-23 11:01:11 +0000

    functions.js notes updated

--- Modern-JavaScript/Functions and Methods/lectures/functions.js
+++ Modern-JavaScript/Functions and Methods/lectures/functions.js
@@ -1,8 +1,13 @@
 // FUNCTIONS
 
-// What are functions?
-
 // NOTES:
 /* 
+// What are functions?
+- as a data type, they fall under OBJECTS
+- they are reusable blocks of code
+- bit like a box with a label on it, we name the function (box) and we can use whatever is inside multiple times
+- good example is when a user presses a button on a webpage, we would want it to run the block of code that is to do with the button as many times as is needed, depending how often the button is pressed
+- Methods and functions are similar but have slight differences which we will learn about later
+
 
 */
