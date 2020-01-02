7e86a624727e7171aa539c7727c4d3d01fc2ea7f
Kat <littlepawcoding@gmail.com>
2020-01-02 21:11:03 +0000

    functions lecture complete - notes updated

--- Modern-JavaScript/Functions and Methods/lectures/functions.js
+++ Modern-JavaScript/Functions and Methods/lectures/functions.js
@@ -12,10 +12,12 @@ greet(); // Hello there
 greet(); // Hello there
 
 // function expression
+// here we have a function stored in this variable called speak, we can call this function the same way as a function declaration
 const speak = function() {
   console.log("Say what?");
 };
 
+// here calling the function expression, same as function declaration except we call the variable name for the function which in this case is speak
 speak();
 
 // NOTES:
@@ -31,7 +33,8 @@ speak();
 - like with variables, we give functions a name
 - a function declaration is a block of code that starts with the keyword function, it also can be hoisted
 - a function expression is a block of code that is wrapped in a separate variable, it cannot be hoisted and is pretty much the only time you see a semi colon end a code block
+- hoisting is loosly described as hoisting the code to the top of the file
 - hoisting is when the funtion is underneath the call. JavaScript looks through the file and hoists up the code to make the call work
 - hoisting does not work for expressions, it would give an error saying the call is undefined
-
+- an expression is any time you get a variable and equal it to something; strings, numbers, functions etc at the end of expressions you should always have semi colons ; (probably the only time you'll see a code block end with an ;)
 */
