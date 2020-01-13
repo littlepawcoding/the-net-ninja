3d3d9c5d1b3011b1048a87733325a2cdadbcf8f1
Kat <littlepawcoding@gmail.com>
2020-01-13 18:03:45 +0000

    return values lecture complete - notes updated

--- Modern-JavaScript/Functions and Methods/lectures/returnValues.js
+++ Modern-JavaScript/Functions and Methods/lectures/returnValues.js
@@ -1,6 +1,31 @@
 // RETURNING VALUES
 
+// function parameter (radius) is a variable local to this code block
+const calcArea = function(radius) {
+  // calculate the area of a cirle - radius
+  // variable area = pi 3.14 multiplied by the radius squared (to the power of 2)
+  // return this area value so it can be used later in the file
+  let area = 3.14 * radius ** 2;
+  // return keyword and area variable
+  // we need to do something with the return value otherwise it won't be displayed
+  return area;
+};
+
+// the return value needs to be stored in a variable (areaTotal) which has global scope
+// normal call to pass through a number but has to be stored in a variable
+const areaTotal = calcArea(5);
+// then log the return variable (areaTotal) to see the result
+console.log(areaTotal); // 78.5
+
+// cleaner way to write the above
+const calculateArea = function(radius) {
+  return 3.14 * radius ** 2;
+};
+const area = calculateArea(5);
+console.log(area); // 78.5
+
 // NOTES:
 /*
+- returning a value allows us to use the function variable globally
 
 */
